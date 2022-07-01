95a86044fc4061ffbc052349cbdaebf86381da82
Yu Xue <54162677+xueyunuist@users.noreply.github.com>
2022-07-01 14:02:13 +0800

    Update FS2.m

--- Problems/FS/FS2.m
+++ Problems/FS/FS2.m
@@ -2,19 +2,6 @@ classdef FS2 < PROBLEM
 % <problem><FS>
 % Constrained benchmark MOP
 
-%------------------------------- Reference --------------------------------
-% Q. Zhang, A. Zhou, S. Zhao, P. N. Suganthan, W. Liu, and S. Tiwari,
-% Multiobjective optimization test instances for the CEC 2009 special
-% session and competition, School of CS & EE, University of Essex, Working
-% Report CES-487, 2009.
-%------------------------------- Copyright --------------------------------
-% Copyright (c) 2018-2019 BIMK Group. You are free to use the PlatEMO for
-% research purposes. All publications which use this platform or any code
-% in the platform should acknowledge the use of "PlatEMO" and reference "Ye
-% Tian, Ran Cheng, Xingyi Zhang, and Yaochu Jin, PlatEMO: A MATLAB platform
-% for evolutionary multi-objective optimization [educational forum], IEEE
-% Computational Intelligence Magazine, 2017, 12(4): 73-87".
-%--------------------------------------------------------------------------
     properties
         dataset;
     end
